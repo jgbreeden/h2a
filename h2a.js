5676f84ee7da2708575e98507faa4c81bad07c70
DarkDog1 <dariang1104@gmail.com>
2022-04-06 14:38:41 -0700

    First commit

---
+++ h2a.js
@@ -0,0 +1,13 @@
+class Employee {
+	constructor(fname, lname, cphone, hphone, address, city, state, zip, status) {
+		this.firstName = fname;
+		this.lastName = lname;
+		this.cellphone = cphone;
+		this.homephone = hphone;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.status = status;
+	}
+}
