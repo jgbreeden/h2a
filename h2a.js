514682f1356c018fd5edead43a1a521aee3b188e
DarkDog1 <dariang1104@gmail.com>
2022-04-14 15:49:31 -0700

    back gount working, sort of

--- h2a.js
+++ h2a.js
@@ -1,4 +1,4 @@
-class Employee {
+class Applicant {
 	constructor(fname, lname, cphone, hphone, address, city, state, zip, status) {
 		this.firstName = fname;
 		this.lastName = lname;
@@ -10,8 +10,11 @@ class Employee {
 		this.zip = zip;
 		this.status = status;
 	}
+	update() {
+		
+	}
 }
-
+var currappl;
 function st_show(tab) {
 	var tabs = document.getElementsByClassName("st_tab");
 	for (let i = 0; i < tabs.length; i++){
@@ -36,7 +39,8 @@ function getEmployees () {
 }
 
 function getEmp(row) {
-	console.log(row.firstChild.innerHTML);
+	//console.log(row.firstChild.innerHTML);
+	getData("st_getEmpDetail.php?id=" + row.firstChild.innerHTML, fillEmpDetail);
 }
 
 function fillEmps(data) {
@@ -52,4 +56,33 @@ function fillEmps(data) {
 	}
 	table.innerHTML = contents;
 	//console.log(data);
+}
+
+function fillEmpDetail(data) {
+	var h = document.getElementById("first");
+	var k = document.getElementById("last");
+	h.value = data.firstname
+	k.value = data.lastname
+	
+	var table = document.getElementById("skillsTab");
+	var contents = "<tr><th>Job skill</th><th>Years</th><th>Where</th></tr>";
+	
+	for (let i = 0; i < data.skills.length; i++){
+		contents += "<tr><td>" + data.skills[i].skillenglish + "</td><td>"
+					+ data.skills[i].years + "</td><td>" + data.skills[i].location
+					+ "</td><td></td></tr>";
+	}
+	currappl = new Applicant(data.firstname, data.lastname, data.phonecell, data.phonehome, data.city, data.state, 0, "new")
+	table.innerHTML = contents;
+}
+function cancelNewApp(){
+	let a = document.getElementById("first").value = currappl.firstName;
+	let b = document.getElementById("last").value = currappl.lastName;
+	let c = document.getElementById("cphone").value = currappl.cellphone;
+	let d = document.getElementById("hphone").value = currappl.homephone;
+	let e = document.getElementById("address").value = currappl.address;
+	let f = document.getElementById("city").value = currappl.city;
+	let g = document.getElementById("state").value = currappl.state;
+	let h = document.getElementById("zip").value = currappl.zip;
+	let i = document.getElementById("status").value = currappl.status;
 }
---
+++ st_getEmpDetail.php
@@ -0,0 +1,31 @@
+<?php
+	//echo '{"test": "test value1"}, {"test": "test value2"}';
+	$conn = new mysqli("localhost", "root", "", "h2a");
+	if ($conn->connect_error) {
+		die("Comunicaton failed: " . $conn->connect_error);
+	}
+	$query = "SELECT applicants.*, experience.years, experience.location, skills.skillenglish "
+			. "FROM applicants INNER JOIN experience ON applicants.id = experience.applicantsid "
+			. "INNER JOIN skills ON experience.skillsid = skills.id WHERE applicants.id =?";
+	$stmt = $conn->prepare($query);
+	$stmt->bind_param("i", $_GET["id"]);
+	$stmt->execute();
+	$results = $stmt->get_result();
+	//echo $results->num_rows;
+	//echo mysqli_error($conn);
+	$row = $results->fetch_assoc();
+	//write person fields, then "skills": [
+	echo '{ "firstname": "' . $row["firstname"] . '", "lastname": "' . $row["lastname"]
+		. '", "cphone": ' . $row["phonecell"] . ', "hphone": ' . $row["phonehome"] 
+		. ', "address": ' . $row["address"] . ', "city": "' . $row["city"]
+		. '", "state": "' . $row["state"] . '", "zip": ' . '0' . ', "status": "' . 'new'
+		. '", "skills": [ { "skillenglish": "' . $row["skillenglish"] . '", "years": '
+		. $row["years"] . ', "location": "' . $row["location"] . '"}';
+	while ($row = $results->fetch_assoc()) {
+		echo ', { "skillenglish": "' . $row["skillenglish"] . '", "years": '
+				. $row["years"] . ', "location": "' . $row["location"] . '"}';
+	}
+	echo ']}'
+	//loop through rows, add skill fields
+	//end record
+?>
