e6587ec65f62e1b906fc396b837a94f3a5ef0f98
DarkDog1 <dariang1104@gmail.com>
2022-04-27 14:46:52 -0700

    start saving and adding companies

---
+++ st_updateComp.php
@@ -0,0 +1,20 @@
+<?php
+	$conn = new mysqli("localhost", "root", "", "h2a");
+	if ($conn->connect_error) {
+		die("Connect error: " . $conn->connect_error);
+	}
+	$sql = "UPDATE employers SET firstname = ?, lastname = ?, phonecell = ?, phonehome = ?,"
+		. "address = ?, city = ?, state = ?, gender = ? WHERE id = ?;";
+	$stmt = $conn->prepare($sql);
+	$g = "male";
+	$stmt->bind_param("ssssssssi", $_POST["fname"], $_POST["lname"], $_POST["cphone"],
+									$_POST["hphone"], $_POST["address"], $_POST["city"],
+									$_POST["state"], $g, $_POST["id"]);
+	$result = $stmt->execute();
+	if ($result == 1) {
+		echo "<h2>Record Saved</h2>";
+		echo "First name: " . $_POST["fname"];
+	} else {
+		echo "There was a problem saving the record, please try again.";
+	}
+?>
