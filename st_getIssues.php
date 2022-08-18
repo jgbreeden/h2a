4315370f6b4a52f2f41afe9e4a787ad317b62b55
Isidro Moreno <morenoisidro00@gmail.com>
2022-08-18 14:32:41 -0700

    added code for documents issues

---
+++ st_getIssues.php
@@ -0,0 +1,21 @@
+[
+<?php
+	//echo '{"test": "test value1"}, {"test": "test value2"}';
+	$conn = new mysqli("localhost", "root", "", "h2a");
+	if ($conn->connect_error) {
+		die("Comunicaton failed: " . $conn->connect_error);
+	}
+	$query = "SELECT * FROM issues ORDER BY issuetype, issueenglish";
+	$result = $conn->query($query);
+	if ($result->num_rows > 0) {
+		$output = "";
+		while ($row = $result->fetch_assoc()) {
+			$output = $output . '{"id": ' . $row["id"]
+							. ', "issueenglish": "' . $row["issueenglish"]
+							. '", "issuetype": "' . $row["issuetype"] . '"},';
+		}
+		$output = substr($output, 0, strlen($output) - 1); //remove trailing comma
+		echo $output;
+	}
+?>
+]
