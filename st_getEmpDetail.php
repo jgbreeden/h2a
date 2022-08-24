77ba12ceb69cb49626ff25453ff3607ad84a1b32
Jacob Parks <jacobparks0124@gmail.com>
2022-08-24 14:40:50 -0700

    showing documents fields

--- st_getEmpDetail.php
+++ st_getEmpDetail.php
@@ -48,18 +48,18 @@
 		$comma = ", ";
 	}
 	echo '], "docs": [';
-	$query = "SELECT ability.id as abid, ability.years, ability.location, ability.percent, "
-			. "ability.details, skills.skillenglish as abeng FROM ability INNER JOIN skills ON "
-			. "ability.skillsid = skills.id WHERE ability.applicantsid =?";
+	$query = "SELECT documents.id as docid, documents.doctype, documents.when, documents.location, "
+			. " issues.issueenglish as doceng FROM documents INNER JOIN issues ON "
+			. "documents.issuesid = issues.id WHERE documents.applicantsid =?";
+
 	$stmt = $conn->prepare($query);
 	$stmt->bind_param("i", $_GET["id"]);
 	$stmt->execute();
 	$comma = "";
 	$results = $stmt->get_result();
 	while ($row = $results->fetch_assoc()) {
-		echo $comma . '{ "abid": "' . $row["abid"] . '", "years": ' . $row["years"] . ', "location": "' 
-			. $row["location"] . '", "percent": ' . $row["percent"] . ', "abeng": "' . $row["abeng"]
-			. '", "details": "' . $row["details"] . '"}';
+		echo $comma . '{ "docid": "' . $row["docid"] . '", "doctype": "' . $row["doctype"] . '", "when": "' 
+			. $row["when"] . '", "location": "' . $row["location"] . '", "doceng": "' . $row["doceng"] . '"}';
 		$comma = ", ";
 	}
 	echo ']}';
