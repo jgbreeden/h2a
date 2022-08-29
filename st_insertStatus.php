48ab6da4df81c640e9d39c479f0062695245e4cf
Isidro Moreno <morenoisidro00@gmail.com>
2022-08-29 15:01:03 -0700

    update status and insert status were made

---
+++ st_insertStatus.php
@@ -0,0 +1,16 @@
+<?php
+	require 'cred.php';
+	$conn = new mysqli($host, $user, $password, "h2a");
+	if ($conn->connect_error) {
+		die("Connect error: " . $conn->connect_error);
+	}
+	$sql = "INSERT INTO documents (issuesid, applicantsid, whengot, location, doctype) VALUES (?, ?, ?, ?, ?);";
+	$stmt = $conn->prepare($sql);
+	$stmt->bind_param("sssss",  $_POST["isid"],  $_POST["apid3"], $_POST["when2"], $_POST["locationdoc2"], $_POST["doctype2"]);
+	$result = $stmt->execute();
+	if ($result == 1) {
+		echo "<h2>Record Saved</h2>";
+	} else {
+		echo "There was a problem saving the record, please try again.";
+	}
+?>
