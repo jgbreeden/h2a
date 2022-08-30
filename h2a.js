fa5c185be2a46874c659fa1bc5f25255ccf6b8e6
Jacob Parks <jacobparks0124@gmail.com>
2022-08-30 15:15:48 -0700

    added health section in insert and update for class issues

--- h2a.js
+++ h2a.js
@@ -76,13 +76,18 @@ class Issues  {
 
 	update() {
 		var formData;
-		if (this.skilltype == "document") {
+		if (this.issuetype == "document") {
 			formData = new FormData(document.getElementById("docform"));
 			sendData(formData, "st_updateDocs.php", showResult);
+		} else if (this.issuetype == "health") {
+			formData = new FormData(document.getElementById("healthsec"));
+			sendData(formData, "st_updateHealth.php", showResult);
 		} else {
 			formData = new FormData(document.getElementById("healthsec"));
 			sendData(formData, "st_updateDocs.php", showResult);
 		}
+		
+		
 	}
 	
 	insert() {
@@ -90,6 +95,9 @@ class Issues  {
 		if (this.issuetype == "document") {
 			formData = new FormData(document.getElementById("docform"));
 			sendData(formData, "st_insertDocs.php", showResult);		
+		} else if (this.issuetype == "health") {
+			formData = new FormData(document.getElementById("healthsec"));
+			sendData(formData, "st_insertHealth.php", showResult);
 		} else {
 			formData = new FormData(document.getElementById("healthsec"));
 			sendData(formData, "st_insertDocs.php", showResult);
