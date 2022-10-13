34c2464ac4ffa6a704adc1125a17f62c36ec88ba
Isidro Moreno <morenoisidro00@gmail.com>
2022-10-13 14:33:38 -0700

    added fields to updateEmp.php

--- st_updateEmp.php
+++ st_updateEmp.php
@@ -7,14 +7,16 @@
 	$sql = "UPDATE applicants SET firstname = ?, lastname = ?, phonecell = ?, phonehome = ?,"
 		. "address = ?, city = ?, state = ?, zipcode = ?, gender = ?, status = ?, yumaonly = ?, travelwhy = ?, stay8mo = ?, overtime = ?,"
 		. "extend = ?, extendwhynot = ?, dateofbirth = ?, email = ?, age = ?, height = ?, weight = ?, maritalstatus = ?,"
-		. "placeofbirth = ?, whatknowvisa = ?, howhearcita = ?, otherhelp = ?, whatknowcita = ?, WHERE id = ?;";
+		. "placeofbirth = ?, whatknowvisa = ?, howhearcita = ?, otherhelp = ?, whatknowcita = ? WHERE id = ?;";
 	$stmt = $conn->prepare($sql);
-	$g = "male";
-	$stmt->bind_param("ssssssssssssssssi", $_POST["fname"], $_POST["lname"], $_POST["cphone"],
+	$stmt->bind_param("sssssssssssssssssssssssssssi", $_POST["fname"], $_POST["lname"], $_POST["cphone"],
 									$_POST["hphone"], $_POST["address"], $_POST["city"],
-									$_POST["state"], $g, $_POST["zip"], $g, $_POST["status"], $_POST["yumaonly"], $_POST["travelwhy"], 
+									$_POST["state"], $_POST["zip"], $_POST["gender"], $_POST["status"], $_POST["yumaonly"], $_POST["travelwhy"], 
 									$_POST["stay8mo"], $_POST["overtime"], $_POST["extend"],
-									$_POST["extendwhynot"], $_POST["id"]);
+									$_POST["extendwhynot"], $_POST["dateofbirth"],  $_POST["email"], $_POST["age"],
+									$_POST["height"], $_POST["weight"], $_POST["maritalstatus"], $_POST["placeofbirth"],
+									$_POST["whatknowvisa"], $_POST["howhearcita"], $_POST["otherhelp"], $_POST["whatknowcita"],
+									$_POST["id"]);
 	$result = $stmt->execute();
 	if ($result == 1) {
 		echo "<h2>Record Saved</h2>";
