1f83097527643e4952528c299f4c343a2c132cd5
Isidro Moreno <morenoisidro00@gmail.com>
2023-01-12 15:01:31 -0700

    added fields to h2ainit.js

--- h2ainit.js
+++ h2ainit.js
@@ -1,2 +1,30 @@
 getData(path + "st_getSkills.php", fillSkill);
 getData(path + "st_getIssues.php", fillIssue);
+document.getElementById("btnNewApps").addEventListener("click", function(){
+    st_show('newApplicants'); 
+    getEmployees('new');
+});
+document.getElementById("btnApps").addEventListener("click", function(){
+    st_show('applicants'); 
+    getEmployees('accepted');
+});
+document.getElementById("btnComp").addEventListener("click", function(){
+    st_show('companies'); 
+    getCompanies();
+});
+document.getElementById("btnAssign").addEventListener("click", function(){
+    st_show('assignments'); 
+    fillCompList();
+});
+document.getElementById("clearApp").addEventListener("click", resetNewApp);
+document.getElementById("saveApp").addEventListener("click", currappl.update);
+document.getElementById("saveSkill").addEventListener("click", saveSkill);
+document.getElementById("clearSkill").addEventListener("click", clearSkill);
+document.getElementById("saveAbility").addEventListener("click", saveAbility);
+document.getElementById("clearAbility").addEventListener("click", clearAbility);
+document.getElementById("saveDoc").addEventListener("click", saveDoc);
+document.getElementById("clearDoc").addEventListener("click", clearDoc);
+document.getElementById("saveHealth").addEventListener("click", saveHealth);
+document.getElementById("clearHealth").addEventListener("click", clearHealth);
+document.getElementById("saveStatus").addEventListener("click", saveStatus);
+document.getElementById("clearStatus").addEventListener("click", clearStatus);
