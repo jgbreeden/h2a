10e3bbad94ecc392682ed3f0d9c327e432ed7251
Logan Johnson <ljfootball600@gmail.com>
2023-11-15 16:56:33 -0700

    added workwhat to st_updateJobs and st_insertJobs

--- st_updateJobs.php
+++ st_updateJobs.php
@@ -5,15 +5,15 @@
 		die("Connect error: " . $conn->connect_error);
 	}
 	$sql = "UPDATE jobhistory SET empname = ?, address = ?, address2 = ?, country = ?, city = ?, state = ?, zip = ?, phone = ?, salary = ?, jobtitle = ?,"
-    			.  "datefrom = ?, dateto = ?, duties = ? WHERE id = ?"; 
+    			.  "datefrom = ?, dateto = ?, duties = ?, whatwork = ? WHERE id = ?"; 
 	$stmt = $conn->prepare($sql);
-	$stmt->bind_param("sssssssssssssi",  $_POST["jcompany"], $_POST["jaddress"], 
+	$stmt->bind_param("ssssssssssssssi",  $_POST["jcompany"], $_POST["jaddress"], 
 										 $_POST["jaddress2"],$_POST["jcity"], 
 										 $_POST["jstate"], $_POST["jzip"],  
 										 $_POST["jphone"], $_POST["jsalary"], 
 										 $_POST["jobtitle"], $_POST["jdatefrom"], 
 										 $_POST["jdateto"],  $_POST["jcountry"], 
-										 $_POST["duties"], $_POST["jid"]);
+										 $_POST["duties"], $_POST["jwhat"], $_POST["jid"]);
 	$result = $stmt->execute();
 	if ($result == 1) {
 		echo "<h2>Record Saved</h2>";
