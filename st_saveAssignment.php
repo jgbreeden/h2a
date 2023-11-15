b28d4016be67b0bd078f33046137457119f31113
Ashley Lua <ashley60006451@gmail.com>
2023-11-15 16:59:59 -0700

    changes

--- st_saveAssignment.php
+++ st_saveAssignment.php
@@ -8,33 +8,41 @@ $new_assignend = date('Y-m-d', strtotime(str_replace('/', '-', $_POST["assignend
 $appid = (int)$_POST["assignappid"];
 $compid = (int)$_POST["assigncomp"];
 
-//save ability record
-$sql = "insert into assignments (applicantsid, employersid, startdate, enddate, assignedby) values (?, ?, ?, ?, ?)";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("iisss", $appid, $compid, $new_assignstart, $new_assignend, $_POST["assignedby"]);
-$result = $stmt->execute();
-if ($result == 1) {
-    echo "record saved";
-} else {
-    echo "error";
-}
+if(isset($_POST["setassign"])){
+    //save ability record
+    $sql = "insert into assignments (applicantsid, employersid, startdate, enddate, assignedby) values (?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("iisss", $appid, $compid, $new_assignstart, $new_assignend, $_POST["assignedby"]);
+    $result = $stmt->execute();
+    if ($result == 1) {
+        echo "record saved";
+    } else {
+        echo "error";
+    }
 
-//update applicant record to status=assigned
-$sql = "UPDATE applicants SET status = 'assigned' WHERE id = ?;";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("i", $appid);
-$result = $stmt->execute();
+    //update applicant record to status=assigned
+    $sql = "UPDATE applicants SET status = 'assigned' WHERE id = ?;";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $appid);
+    $result = $stmt->execute();
 
-$sql = "SELECT * FROM appds160 WHERE applicantsid = ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("i", $appid);
-$stmt->execute();
-$results = $stmt->get_result();
-if (!($row = $results->fetch_assoc())) {
-    $sql = "INSERT INTO appds160 (applicantsid) values(?)";
+    $sql = "SELECT * FROM appds160 WHERE applicantsid = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("i", $appid);
     $stmt->execute();
+    $results = $stmt->get_result();
+    if (!($row = $results->fetch_assoc())) {
+        $sql = "INSERT INTO appds160 (applicantsid) values(?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("i", $appid);
+        $stmt->execute();
+    }
+}
+if(isset($_POST["linkcompany"])){
+    $sql = "UPDATE applicants SET employorsid = ? WHERE id = ?;";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ii", $compid, $appid);
+    $result = $stmt->execute();
 }
 $conn->close();
 ?>
