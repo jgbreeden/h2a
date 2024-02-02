058fe806037486a73b49284154fbcea28bcd3361
Jacob Deanda <iseepolar@gmail.com>
2024-02-01 17:54:59 -0700

    added new file and changes to h2a js and html

---
+++ st_exportApps.php
@@ -0,0 +1,9 @@
+<?php
+	header('Expires: Thu, 1 Jan 1970 00:00:00 GMT');
+	header('Pragma: no-cache');
+	header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+	header('Cache-Control: post-check=0, pre-check=0',false);
+	header('Content-Type: application/json; charset=utf-8');
+	require 'cred.php';
+    echo "testing";
+?>
